043132cbe116d88c5ce329a784308a7a02e35e32
vladimirmiloradov <miloradovsinox@gmail.com>
2023-06-27 13:14:04 +0300

    4 commit

--- 201_351_Miloradov_stage_1/main.cpp
+++ 201_351_Miloradov_stage_1/main.cpp
@@ -10,29 +10,42 @@
 #include <QFile>
 #include <QDebug>
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    //Загрузка массива из JSON
+    QFile jsonFile("C:/Users/SIXTECH/Desktop/201_351_Miloradov/cridentials.json");
+    if (!jsonFile.open(QIODevice::ReadOnly ))
+    {
+        return 0;
+    }
+    QByteArray saveData = jsonFile.readAll();
+    QJsonDocument jsonDocument(QJsonDocument::fromJson(saveData));
+    QJsonObject data = jsonDocument.object();
+
+    //Парсинг в списки
+    QStringList Logins;
+    QStringList Passwords;
+    QJsonArray array = data["cridentials"].toArray();
+    foreach (const QJsonValue & value, array) {
+        QJsonObject obj = value.toObject();
+        Logins.append(obj["login"].toString());
+        Passwords.append(obj["password"].toString());
+    }
+
+    //Запрос пинкода
     QTextStream cin(stdin);
     int pin(0);
-    std::cout << "Enter pincode: ";
+    qDebug() << "Enter pincode: ";
     std::cin >> pin;
+
+    //Если введён верный пинкод
     if (pin == 1234)
     {
         while (true)
         {
-            QFile jsonFile("C:/Users/SIXTECH/Desktop/201_351_Miloradov/cridentials.json");
-            if (!jsonFile.open(QIODevice::ReadOnly ))
-            {
-                return 0;
-            }
-            QByteArray saveData = jsonFile.readAll();
-            QJsonDocument jsonDocument(QJsonDocument::fromJson(saveData));
-            QJsonObject data = jsonDocument.object();
-            QJsonArray array = data["cridentials"].toArray();
-            std::cout << "Enter id of social media account to get log/pass or -1 to exit: ";
+            qDebug() << "Enter id of social media account to get log/pass or -1 to exit: ";
             QTextStream cin(stdin);
             int id_user(0);
             std::cin >> id_user;
@@ -42,14 +55,18 @@ int main(int argc, char *argv[])
             }
             else if (id_user > 2)
             {
-                std::cout << "User with such id doesn't exist. Try another id.";
+                qDebug() << "User with such id doesn't exist. Try another id.";
             }
             else
             {
-                QJsonValueRef output = array[id_user];
-                qDebug() << output;
+                qDebug() << "Login: " << Logins[id_user];
+                qDebug() << "Password " << Passwords[id_user];
             }
         }
+    }
+    else
+    {
+        qDebug() << "Incorrect pin. Try it again!";
     }
     return a.exec();
 }
